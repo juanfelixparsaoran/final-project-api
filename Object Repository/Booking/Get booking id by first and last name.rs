22766d1d902162798d6127fce0ec73a3548b4a04
Juan <095085@LAPTOP-BRAVO15.bfi.co.id>
2022-07-12 16:22:05 +0700

    add tc for get booking by first and last name

--- Object Repository/Booking/Get booking id by first and last name.rs
+++ Object Repository/Booking/Get booking id by first and last name.rs
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>Get booking by id</name>
+   <name>Get booking id by first and last name</name>
    <tag></tag>
-   <elementGuidId>1e942f13-89e4-4b40-a05a-f6a0da6496f5</elementGuidId>
+   <elementGuidId>214693b9-c7b6-4ebf-9140-4733f742f966</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <connectionTimeout>0</connectionTimeout>
@@ -17,13 +17,13 @@
       <name>Accept</name>
       <type>Main</type>
       <value>application/json</value>
-      <webElementGuid>a656019e-606b-45dc-befe-451be9a485ed</webElementGuid>
+      <webElementGuid>292bcc4d-e34f-445d-a599-6b63fd1c20fd</webElementGuid>
    </httpHeaderProperties>
    <katalonVersion>8.3.5</katalonVersion>
    <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://restful-booker.herokuapp.com/booking/2383</restUrl>
+   <restUrl>https://restful-booker.herokuapp.com/booking?firstname=Sally&amp;lastname=Brown</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -45,12 +45,10 @@ import internal.GlobalVariable as GlobalVariable
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
 ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
-WS.verifyElementPropertyValue(response, 'firstname', &quot;Irene&quot;)
-WS.verifyElementPropertyValue(response, 'lastname', &quot;Jiménez&quot;)
-WS.verifyElementPropertyValue(response, 'totalprice', 211)
-WS.verifyElementPropertyValue(response, 'depositpaid', true)
-WS.verifyElementPropertyValue(response, 'bookingdates.checkin', &quot;2022-07-11&quot;)
-WS.verifyElementPropertyValue(response, 'bookingdates.checkout', &quot;2022-07-22&quot;)
-WS.verifyElementPropertyValue(response, 'additionalneeds', &quot;Breakfast&quot;)</verificationScript>
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
